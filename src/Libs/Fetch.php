f436cfbbdb37e08eef09c84ed1eceefc5b5539cf
haveapland <haveapland@gmail.com>
2015-08-31 18:50:57 +0300

    some refactoring

--- src/Libs/Fetch.php
+++ src/Libs/Fetch.php
@@ -4,11 +4,13 @@ namespace haveapland\EvePrice\Libs;
 
 class Fetch
 {
-
- private $itemlist;
- private $apiurl_json = 'http://api.eve-central.com/api/marketstat/json';
- private $postfield;
- private $json_data;
+    private $itemlist;
+    private $api_url = [
+                'xml' => 'http://api.eve-central.com/api/marketstat/',
+                'json' => 'http://api.eve-central.com/api/marketstat/json'
+            ];
+    private $post_field;
+    private $data;
 
     public function __construct($itemlist, $systemid=30000142, $hours=24)
     {
@@ -28,19 +30,24 @@ class Fetch
 
     public function getPostfield()
     {
-        return $this->postfield;
+        return $this->post_field;
+    }
+
+    public function getJsonData()
+    {
+        return $this->json_data;
     }
 
     private function generatePostFields($typeid = [], $systemid=30000142, $hours=24)
     {
-        $this->postfield = 'hours='.$hours.'&usesystem='.$systemid;
+        $this->post_field = 'hours='.$hours.'&usesystem='.$systemid;
     
         foreach ($typeid as $id)
         {
-            $this->postfield .= '&typeid='.$id;
+            $this->post_field .= '&typeid='.$id;
         }
 
-        if(!$this->postfield){
+        if(!$this->post_field){
             throw new \Exception('Empty POST request');
         }
 
@@ -49,23 +56,24 @@ class Fetch
 
     public function getData()
     {
+
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, $this->apiurl_json);
+        curl_setopt($ch, CURLOPT_URL, $this->api_url['json']);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS,
-                            $this->postfield);
+                            $this->post_field);
 
-        $this->json_data = json_decode(curl_exec ($ch));
-
+        $this->data = json_decode(curl_exec ($ch));
+        
         curl_close ($ch); 
 
-        if(!$this->json_data){
+        if(!$this->data){
             throw new \Exception('Empty data from eveCentral');
         }
 
-        return $this->json_data;
+        return true;
     }
 
 }
--- tests/FetchTest.php
+++ tests/FetchTest.php
@@ -11,13 +11,17 @@ class FetchTest extends \PHPUnit_Framework_TestCase
 
         $itemlist = [178,179,180,181,182,183,184,185,186,187];
         $this->fetch = new Fetch($itemlist);
-
     }
 
     public function testGeneratePostFields()
     {
         $post_string = 'hours=24&usesystem=30000142&typeid=178&typeid=179&typeid=180&typeid=181&typeid=182&typeid=183&typeid=184&typeid=185&typeid=186&typeid=187';
         $this->assertEquals($post_string, $this->fetch->getPostfield());
+    }
+
+    public function testGetData()
+    {
+        $this->assertTrue($this->fetch->getData());
     }
 
 }
